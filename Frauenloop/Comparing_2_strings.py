91eb8737fc3ecfe5c4d8458b684ebbf15b60b115
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2021-02-11 18:15:02 +0100

    Add files via upload

---
+++ Frauenloop/Comparing_2_strings.py
@@ -0,0 +1,35 @@
+# Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+# When positives and positives interact, they remain positive.
+# When negatives and negatives interact, they remain negative.
+# But when negatives and positives interact, they become neutral, and are shown as the number 0.The two strings will be the same length.
+
+# neutralise("+-+", "+--") ➞ "+-0"
+# Compare the first characters of each string, then the next in turn.
+
+
+
+
+
+from os import EX_DATAERR
+
+
+def comparing_strings():
+    result = ""
+    word_1 = "--++-+--++"
+    word_2 = "-+-++--++-"
+
+    for i in range(len(word_1)):
+        if word_1[i] == word_2[i]:
+            result = result+(word_1[i])
+        else:
+            result = result+("0")
+    return result
+
+end= comparing_strings()
+print(end)
+
+
+
+
+   
